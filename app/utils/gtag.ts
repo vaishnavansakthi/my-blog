398e8c64d9ca90d3c2b98627cdefb4328dad6654
Vaishnavan M <145651333+vaishnavansakthi@users.noreply.github.com>
2025-11-02 14:27:00 +0530

    fix: adding google tag

--- app/utils/gtag.ts
+++ app/utils/gtag.ts
@@ -1,10 +1,17 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 // lib/gtag.ts
+
+declare global {
+  interface Window {
+    gtag: (...args: any[]) => void;
+  }
+}
+
 export const GA_ID = "G-H952GR6KXJ";
 
 // Track page views
 export const pageview = (url: string) => {
-  if (typeof window !== "undefined" && GA_ID) {
+  if (typeof window !== "undefined" && GA_ID && typeof window.gtag === "function") {
     window.gtag("config", GA_ID, {
       page_path: url,
     });
@@ -13,7 +20,7 @@ export const pageview = (url: string) => {
 
 // Track specific events
 export const event = ({ action, category, label, value }: any) => {
-  if (typeof window !== "undefined" && GA_ID) {
+  if (typeof window !== "undefined" && GA_ID && typeof window.gtag === "function") {
     window.gtag("event", action, {
       event_category: category,
       event_label: label,
